dd4c9040086f6dfc774cb23244e8d26cbde8c1df
Gaya Mehenni <gaya.mehenni@polymtl.ca>
2021-02-06 14:33:07 -0500

    Update

--- lib/widgets/watch_out.dart
+++ lib/widgets/watch_out.dart
@@ -8,9 +8,18 @@ class WatchOut extends StatefulWidget {
 class _WatchOutState extends State<WatchOut> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      color: Colors.blue,
+    return Center(
+      heightFactor: 1,
+      child: Text(
+        'You should be careful with your potatoes'.toUpperCase(),
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+        softWrap: true,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
